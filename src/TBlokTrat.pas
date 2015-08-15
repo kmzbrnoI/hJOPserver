e2c2d410ffcf22585e8e8af5e7e8c0788d7b6923
Jan Horacek <jan.horacek@seznam.cz>
2015-08-16 00:48:01 +0200

    Opraven BUG zpusobujici pad SW pri nacitani trati, ktera obsahuje referenci na neexistujici TU.

--- src/TBlokTrat.pas
+++ src/TBlokTrat.pas
@@ -715,6 +715,7 @@ begin
     begin
      writelog('Trat '+Self.GetGlobalSettings().name+' obsahuje referenci na TU ID '+IntToStr(Self.TratSettings.Useky[i])+', tento blok ale bud neexistuje, nebo neni typu TU, odstranuji referenci', WR_ERROR);
      Self.TratSettings.Useky.Delete(i);
+     continue;
     end;
    if (((Blk as TBlkTU).InTrat <> -1) and ((Blk as TBlkTU).InTrat <> Self.GetGlobalSettings().id)) then
     begin
