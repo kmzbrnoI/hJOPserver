d53ef9ac99d89601efb5c6ad5253adbd905fb846
Jan Horacek <jan.horacek@seznam.cz>
2016-10-22 11:38:09 +0200

    Utility PT serveru do vlastni unity.

---
+++ src/PTUtils.pas
@@ -0,0 +1,56 @@
+unit PTUtils;
+
+{
+  Tato unita obsahuje uzitecne funkce pro PTserver a unity vyuzivajici
+  funkcionalitu PTserveru.
+}
+
+interface
+
+uses JsonDataObjects, Generics.Collections, Classes, Character;
+
+procedure PtErrorToJson(json:TJsonObject; httpCode:string; title:string; detail:string = '');
+procedure HttpParametersToDict(params:TStrings; var dict:TDictionary<string, string>);
+function HttpParamToBool(value:string):boolean;
+
+implementation
+
+////////////////////////////////////////////////////////////////////////////////
+
+procedure PtErrorToJson(json:TJsonObject; httpCode:string; title:string; detail:string = '');
+begin
+ json['code']  := httpCode;
+ json['title'] := title;
+ if (detail <> '') then json['detail'] := detail;
+end;
+
+////////////////////////////////////////////////////////////////////////////////
+
+procedure HttpParametersToDict(params:TStrings; var dict:TDictionary<string, string>);
+var param:string;
+    splitted:TStrings;
+begin
+ splitted := TStringList.Create();
+ try
+   dict.Clear();
+   for param in params do
+    begin
+     splitted.Clear();
+     ExtractStrings(['='], [], PChar(param), splitted);
+     if (splitted.Count = 2) then dict.Add(splitted[0], splitted[1]);
+    end;
+ finally
+   splitted.Free();
+ end;
+end;
+
+////////////////////////////////////////////////////////////////////////////////
+
+function HttpParamToBool(value:string):boolean;
+begin
+ Result := (ToLower(value) = 'true') or (value = '1');
+end;
+
+////////////////////////////////////////////////////////////////////////////////
+
+end.
