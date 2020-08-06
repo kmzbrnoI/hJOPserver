9ef6071aea834fe71172fdf0bceaad3b9200803d
Jan Horacek <me@apophis.cz>
2020-08-06 10:32:58 +0200

    Pridat try-finally blok.

--- src/pt/PTEndpointBlok.pas
+++ src/pt/PTEndpointBlok.pas
@@ -76,9 +76,12 @@ function TPTEndpointBlok.EndpointMatch(path:string):boolean;
 var re: TJclRegEx;
 begin
  re := TJclRegEx.Create();
- re.Compile(_ENDPOINT_MATCH_REGEX, false);
- Result := re.Match(path);
- re.Free();
+ try
+   re.Compile(_ENDPOINT_MATCH_REGEX, false);
+   Result := re.Match(path);
+ finally
+   re.Free();
+ end;
 end;
 
 ////////////////////////////////////////////////////////////////////////////////
