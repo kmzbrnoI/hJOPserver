d909028b231e464d2bc54501a55c9f32024d9b7a
Jan Malina <me@apophis.cz>
2025-10-21 22:07:02 +0200

    Fix loading of TAreaLighting.

--- src/areas/AreaLighting.pas
+++ src/areas/AreaLighting.pas
@@ -28,8 +28,24 @@ begin
 end;
 
 procedure TAreaLighting.Parse(spnlitem: string);
+var strs: TStrings;
 begin
-  Self.rcsAddr.Load(spnlitem);
+  strs := TStringList.Create();
+  try
+    ExtractStringsEx(['|', ','], [], spnlitem, strs);
+    if (strs.Count = 3) then
+    begin
+      Self.name := strs[2];
+      Self.rcsAddr := TRCSs.RCSsAddr(0, StrToInt(strs[0]), StrToInt(strs[1]));
+    end else if (strs.Count = 4) then
+    begin
+      Self.name := strs[3];
+      Self.rcsAddr := TRCSs.RCSsAddr(StrToInt(strs[0]), StrToInt(strs[1]), StrToInt(strs[2]));
+    end else
+      raise Exception.Create('Light number of items is not 3 or 4');
+  finally
+    strs.Free();
+  end;
 end;
 
 function TAreaLighting.GetActive(): Boolean;
